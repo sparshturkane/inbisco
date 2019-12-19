0deec8663c48accade974047d439c6f08983acbd
sparshturkane <rajaturkane99@gmail.com>
2019-12-19 15:51:17 +0530

    footer done

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
         <div class="footer_top">
             <div class="container-fluid p-0">
                 <div class="row no-gutters ">
-                    <div class="col-xl-3 col-12 col-md-4">
+                    <!-- <div class="col-xl-3 col-12 col-md-4">
                         <div class="footer_widget">
                             <div class="footer_logo">
                                 <a href="index.html">
@@ -21,8 +21,8 @@
                                 </li>
                             </ul>
                         </div>
-                    </div>
-                    <div class="col-xl-9 col-12 col-md-8">
+                    </div> -->
+                    <div class="col-xl-12 col-12 col-md-12">
                         <!-- <div class="footer_header d-flex justify-content-between">
                             <div class="footer_header_left">
                                 <h3>Do youn have any project or Query ?</h3>
@@ -33,23 +33,23 @@
                             </div>
                         </div> -->
                         <div class="row">
-                            <div class="col-xl-8 col-12 col-md-12">
+                            <div class="col-xl-12 col-12 col-md-12">
                                 <div class="row">
-                                    <div class="col-xl-4 col-12 col-md-4">
+                                    <div class="col-xl-2 col-12 col-md-2">
                                         <div class="footer_widget">
                                             <h3 class="footer_heading">
                                                 About
                                             </h3>
                                             <ul class="quick_links">
-                                                <li><a href="#">Our Values</a></li>
-                                                <li><a href="#">Vison & Purpose</a></li>
-                                                <li><a href="#">Our Presence</a></li>
-                                                <li><a href="#">Awards</a></li>
-                                                <li><a href="#">Global Group</a></li>
+                                                <li><a href="http://inbiscoindia.com/our-values/">Our Values</a></li>
+                                                <li><a href="http://inbiscoindia.com/vision-and-purpose">Vison & Purpose</a></li>
+                                                <li><a href="http://inbiscoindia.com/our-presence">Our Presence</a></li>
+                                                <li><a href="http://inbiscoindia.com/awards">Awards</a></li>
+                                                <li><a href="https://mayora.com/">Global Group</a></li>
                                             </ul>
                                         </div>
                                     </div>
-                                    <div class="col-xl-4 col-12 col-md-4">
+                                    <div class="col-xl-2 col-12 col-md-2">
                                         <div class="footer_widget">
                                             <h3 class="footer_heading">
                                                 Our Brands
@@ -58,17 +58,53 @@
                                                 <li><a href="#">Kopiko</a></li>
                                                 <li><a href="#">Juizy Milk</a></li>
                                                 <li><a href="#">Choki Choki</a></li>
+                                                <li><a href="#">Joymee</a></li>
+                                                <li><a href="#">CalCheese</a></li>
+                                                <li><a href="#">Danisa</a></li>
                                             </ul>
                                         </div>
                                     </div>
-                                    <div class="col-xl-4 col-12 col-md-4 ">
+                                    <div class="col-xl-2 col-12 col-md-2 ">
                                         <div class="footer_widget">
                                             <h3 class="footer_heading">
                                                 Business Relations
                                             </h3>
                                             <ul class="quick_links">
-                                                <li><a href="#">Superior </a></li>
-                                                <li><a href="#"> Community</a></li>
+                                                <li><a href="http://inbiscoindia.com/suppliers">Supplier</a></li>
+                                                <li><a href="http://inbiscoindia.com/distributors">Distributor</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-2 col-12 col-md-2 ">
+                                        <div class="footer_widget">
+                                            <h3 class="footer_heading">
+                                                CSR
+                                            </h3>
+                                            <ul class="quick_links">
+                                                <li><a href="http://inbiscoindia.com/community">Community</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-2 col-12 col-md-2 ">
+                                        <div class="footer_widget">
+                                            <h3 class="footer_heading">
+                                                CAREER
+                                            </h3>
+                                            <ul class="quick_links">
+                                                <li><a href="http://inbiscoindia.com/join-us-as-a-professional/">Join us as a professional</a></li>
+                                                <li><a href="http://inbiscoindia.com/start-your-career-with-us/">Start your career with us</a></li>
+                                                <li><a href="http://inbiscoindia.com/being-part-of-team-inbisco/">Being part of team Inbisco</a></li>
+                                                <li><a href="http://inbiscoindia.com/jobs-inbisco/">Jobs@INBISCO</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-2 col-12 col-md-2 ">
+                                        <div class="footer_widget">
+                                            <h3 class="footer_heading">
+                                                MEDIA
+                                            </h3>
+                                            <ul class="quick_links">
+                                                <li><a href="http://inbiscoindia.com/news-and-events/">News & Events</a></li>
                                             </ul>
                                         </div>
                                     </div>
