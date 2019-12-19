d79c14b5a860e93abf9c3ec851ede772c50ee263
sparshturkane <rajaturkane99@gmail.com>
2019-12-19 15:33:11 +0530

    added php code

---
+++ footer.php
@@ -0,0 +1,115 @@
+<footer class="footer footer_bg">
+        <div class="footer_top">
+            <div class="container-fluid p-0">
+                <div class="row no-gutters ">
+                    <div class="col-xl-3 col-12 col-md-4">
+                        <div class="footer_widget">
+                            <div class="footer_logo">
+                                <a href="index.html">
+                                    <img src="img/footer-logo1.png" alt="">
+                                </a>
+                            </div>
+                            <ul class="social_links">
+                                <li>
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-instagram"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-9 col-12 col-md-8">
+                        <!-- <div class="footer_header d-flex justify-content-between">
+                            <div class="footer_header_left">
+                                <h3>Do youn have any project or Query ?</h3>
+                                <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt. </p>
+                            </div>
+                            <div class="footer_btn">
+                                <a href="#" class="boxed-btn2">Contact Us</a>
+                            </div>
+                        </div> -->
+                        <div class="row">
+                            <div class="col-xl-8 col-12 col-md-12">
+                                <div class="row">
+                                    <div class="col-xl-4 col-12 col-md-4">
+                                        <div class="footer_widget">
+                                            <h3 class="footer_heading">
+                                                About
+                                            </h3>
+                                            <ul class="quick_links">
+                                                <li><a href="#">Our Values</a></li>
+                                                <li><a href="#">Vison & Purpose</a></li>
+                                                <li><a href="#">Our Presence</a></li>
+                                                <li><a href="#">Awards</a></li>
+                                                <li><a href="#">Global Group</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-4 col-12 col-md-4">
+                                        <div class="footer_widget">
+                                            <h3 class="footer_heading">
+                                                Our Brands
+                                            </h3>
+                                            <ul class="quick_links">
+                                                <li><a href="#">Kopiko</a></li>
+                                                <li><a href="#">Juizy Milk</a></li>
+                                                <li><a href="#">Choki Choki</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-4 col-12 col-md-4 ">
+                                        <div class="footer_widget">
+                                            <h3 class="footer_heading">
+                                                Business Relations
+                                            </h3>
+                                            <ul class="quick_links">
+                                                <li><a href="#">Superior </a></li>
+                                                <li><a href="#"> Community</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer_copy_right">
+            <!-- <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p> -->
+        </div>
+    </footer>
+    <!-- footer_end -->
+
+
+    <!-- JS here -->
+    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/ajax-form.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="js/scrollIt.js"></script>
+    <script src="js/jquery.scrollUp.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/nice-select.min.js"></script>
+    <script src="js/jquery.slicknav.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/plugins.js"></script>
+
+    <!--contact js-->
+    <script src="js/contact.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.form.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/mail-script.js"></script>
+
+    <script src="js/main.js"></script>
---
+++ header.php
@@ -0,0 +1,51 @@
+<header>
+        <div class="header-area ">
+            <div id="sticky-header" class="main-header-area white-bg">
+                <div class="container-fluid p-0">
+                    <div class="row align-items-center justify-content-between no-gutters">
+                        <div class="col-xl-2 col-lg-2">
+                            <div class="logo-img">
+                                <a href="index.html">
+                                    <img src="img/logo1.png" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-7 col-lg-7">
+                            <div class="main-menu  d-none d-lg-block">
+                                <nav>
+                                    <ul id="navigation">
+                                        <li><a class="active" href="index.html">About</a></li>
+                                        <li><a href="about.html">Our Brands</a></li>
+                                        <li><a href="project.html">Business Relations</a></li>
+                                        <li><a href="service.html">CSR</a></li>
+
+                                        <li><a href="#">Career <i class="ti-angle-down"></i></a>
+                                            <ul class="submenu">
+                                                <li><a href="blog.html">blog</a></li>
+                                                <li><a href="single-blog.html">single-blog</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Media <i class="ti-angle-down"></i></a>
+                                            <ul class="submenu">
+                                                <li><a href="project-details.html">project-details</a></li>
+                                                <li><a href="elements.html">elements</a></li>
+                                            </ul>
+                                        </li>
+                                        <!-- <li><a href="contact.html">Contact</a></li> -->
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <!-- <div class="col-xl-3 col-lg-3 d-none d-lg-block">
+                            <div class="get_in_tauch">
+                                <a href="#" class="boxed-btn">GET IN TOUCH</a>
+                            </div>
+                        </div> -->
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
